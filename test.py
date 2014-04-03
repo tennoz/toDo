765f8c6f5cec3194e1ad152f9dbf27320af461b7
mad <mahdi.cie@gmail.com>
2014-04-03 18:26:34 +0200

    main class

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print "Test"
+#!/usr/bin/env python
+import pygtk
+import gtk
+
+
+class MyProgram:
+ 
+    def __init__(self):
+        # create a new window
+        app_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        app_window.set_title("MyProgram title")
+
+        app_window.show()
+        return
+
+
+def main():
+    gtk.main()
+    return 0
+
+if __name__ == "__main__":
+    MyProgram()
+    main()
